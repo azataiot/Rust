f0209d6eac68bfe69310ac3cfd9eb1aaa7d5e13e
Azat <8280770+azataiot@users.noreply.github.com>
2025-09-04 18:08:55 +0300

    the simple docker is working

--- rust-musl-cross/src/main.rs
+++ rust-musl-cross/src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Server started and listening on: 0.0.0.0:3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    println!("Server started and listening on: 0.0.0.0:8000");
     axum::serve(listener, app).await.unwrap();
 }
